231c4fb96ab0bc51d96388c0aca2f4a5384a049a
MiguelNS <MiguelNS101@gmail.com>
2022-08-17 21:10:00 -0300

    Fix navigate

--- __tests__/end-to-end/actions/common/navigate.js
+++ __tests__/end-to-end/actions/common/navigate.js
@@ -59,7 +59,7 @@ const navigate = {
     homepage: async (page, role) => {
       const { homePage } = user[role]();
 
-      // await expect(page).toNavigate(`${host}/login`);
+      await expect(page).toNavigate(`${host}/login`);
       await expect(page).toCompareURL(`${host}${homePage}`);
        return true;
     },
